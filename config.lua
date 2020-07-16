0f27055260133eb3642006ad6cd8514dbb59aa98
Rabia Alhaffar <steria773@gmail.com>
2020-07-16 03:58:18 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -5,48 +5,47 @@ load_directory = "resources"
 
 config = {
     options = {
-	    width = 800,
-		height = 600,
-		title = "TAIKO",
+	width = 800,
+	height = 600,
+	title = "TAIKO",
         vsync = true,
-	    antialiasing = false,
-	    highdpi = true,
-		dev = true,
+	antialiasing = false,
+	highdpi = true,
+	dev = true,
+	background_color = rl.BLACK -- Background color
+    },
+	
+    -- Controls
+    keys = {
+	up = rl.KEY_UP,
+	down = rl.KEY_DOWN,
+	left = rl.KEY_LEFT,
+	right = rl.KEY_RIGHT
+    },
+	
+    resources = {
+        hit_sound = rl.LoadSound(load_directory.."/13909__adcbicycle__5.wav"),
+	miss_sound = rl.LoadSound(load_directory.."/515625__mrickey13__throw-swipe.wav"),
+	background_image = nil, -- Use rl.LoadTexture(src)
+	song = nil, -- Use rl.LoadMusicStream(src)
 		
-		background_color = rl.BLACK -- Background color
-	},
+	-- Use rl.LoadTexture(src) for each one
+	up = nil,
+	down = nil,
+	left = nil,
+	right = nil
+    },
 	
-	-- Controls
-	keys = {
-	    up = rl.KEY_UP,
-		down = rl.KEY_DOWN,
-		left = rl.KEY_LEFT,
-		right = rl.KEY_RIGHT
-	},
+    circles = {
+	first_color = rl.RED,
+	second_color = rl.BLUE,
+	third_color = rl.GREEN,
+	fourth_color = rl.PURPLE,
+	size = 50,
+	margin = 2,
+	speed = 6,
+    },
 	
-	resources = {
-	    hit_sound = rl.LoadSound(load_directory.."/13909__adcbicycle__5.wav"),
-		miss_sound = rl.LoadSound(load_directory.."/515625__mrickey13__throw-swipe.wav"),
-		background_image = nil, -- Use rl.LoadTexture(src)
-		song = nil, -- Use rl.LoadMusicStream(src)
-		
-		-- Use rl.LoadTexture(src) for each one
-		up = nil,
-		down = nil,
-		left = nil,
-		right = nil
-	},
-	
-	circles = {
-	    first_color = rl.RED,
-		second_color = rl.BLUE,
-		third_color = rl.GREEN,
-		fourth_color = rl.PURPLE,
-		size = 50,
-		margin = 2,
-		speed = 6,
-	},
-	
-	level = nil,
-	hit_score = 100
+    level = nil,
+    hit_score = 100
 }
